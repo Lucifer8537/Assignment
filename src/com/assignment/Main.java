0a2be248489d891485a21e247edec3cee0aa0c7f
navodit <navodit50@gmail.com>
2024-09-03 20:41:26 +0530

    Added code for resolving delimeter at start

--- src/com/assignment/Main.java
+++ src/com/assignment/Main.java
@@ -3,10 +3,16 @@ package com.assignment;
 public class Main {
 	public long Add(String numbers) {
 		if(numbers.length() > 0) {
-			String[] numberArr = numbers.split("[,\n]");
+			String delimeter = ",\n";
+			if(numbers.startsWith("//") && numbers.length() > 2) {
+				String[] delimeterArr = numbers.split("[\n]");
+				delimeter = delimeterArr[0].substring(2);
+				numbers = delimeterArr[1];
+			}
+			String[] numberArr = numbers.split("["+delimeter+"]");
 			long sum = 0;
-			for(int i = 0; i < numberArr.length; i++) sum += Long.parseLong(numberArr[i].trim()); 
-			return sum;
+			for(int i = 0; i < numberArr.length; i++) sum += Long.parseLong(numberArr[i].trim());
+			return sum;	
 		}
 		return 0;
 	}
